968f156b00b8910cd2565239a998b804dadf783d
AlenaDvo <131749753+AlenaDvo@users.noreply.github.com>
2023-05-19 19:27:42 +0200

    Corrected
    description stripe
    current photo

--- script.js
+++ script.js
@@ -96,27 +96,30 @@ let numberOfPhotos = 17;
 let lastlySeenPhoto = 0;
 
 imagesData.forEach((element) => {
-    $('.thumbnail-container').append('<div class="thumbnail"> <img src="' + element.photo + '" alt="' + element.title + '"' + 'data-number="' + imagesData.indexOf(element) + '">'
+    $('.thumbnail-container').append('<div class="thumbnail" data-number="' + imagesData.indexOf(element) + '"><img src="' + element.photo + '" alt="' + element.title + '">'
         + '<div class="thumbnail-title">' + element.title + '</div><img src="triangle_up.svg" class="triangle-up"></div>');
 });
 
 $('.thumbnail-container').on('click', '.thumbnail', (event) => {
-    currentPhoto = parseInt($(event.target).attr('data-number'));
+    // console.log(event.currentTarget);
+    // console.log(event);
+    currentPhoto = parseInt($(event.currentTarget).attr('data-number'));
     loadPhoto(currentPhoto);
 });
 
 function loadPhoto(photoNumber) {
+    console.log(photoNumber);
     $('#photo').attr('src', imagesData[photoNumber].photo);
     $('#photo').attr('alt', imagesData[photoNumber].title);
     $('#photo-title').text(imagesData[photoNumber].title);
     $('#photo-description').text(imagesData[photoNumber].description);
-    // if (photoNumber !== lastlySeenPhoto) {
-    //     $('.thumbnail[data-number="' + lastlySeenPhoto + '"]').css('border', '5px double white');
-    //     // $('.thumbnail[data-number="' + lastlySeenPhoto + '"]').css('border', '5px double white');
-    // };
+    if (photoNumber !== lastlySeenPhoto) {
+        // $('.thumbnail[data-number="' + lastlySeenPhoto + '"]').css('border', 'red');
+        $('.thumbnail[data-number="' + lastlySeenPhoto + '"]').css('border', '5px double white');
+    };
     $('.thumbnail[data-number="' + photoNumber + '"]').css('border', '5px double black');
-    $('.thumbnail[data-number="' + photoNumber + '"]').append("<p>blalblajdklkj</p>");
-    console.log($('.thumbnail[data-number="' + photoNumber + '"]'));
+    // $('.thumbnail[data-number="' + photoNumber + '"]').append("<p>blalblajdklkj</p>");
+    // console.log($('.thumbnail[data-number="' + photoNumber + '"]'));
     lastlySeenPhoto = photoNumber;
     // $('.thumbnail.00').css('border', '5px double black');
     // event.target.parentNode.style = 'text-decoration: line-through';
